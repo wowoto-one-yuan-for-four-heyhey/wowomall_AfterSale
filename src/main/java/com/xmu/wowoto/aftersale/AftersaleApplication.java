cf557d40b0e38aacb8de1d9add1152eaaad847dc
medalwill <42199368+medalwill@users.noreply.github.com>
2019-12-04 16:00:12 +0800

    1
    1

---
+++ src/main/java/com/xmu/wowoto/aftersale/AftersaleApplication.java
@@ -0,0 +1,13 @@
+package com.xmu.wowoto.aftersale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftersaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftersaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xmu/wowoto/aftersale/AftersaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmu.wowoto.aftersale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftersaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
