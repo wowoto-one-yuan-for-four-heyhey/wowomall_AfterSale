71200453bdb3ac054efdaa5d298bbce4a548f9a9
medalwill <42199368+medalwill@users.noreply.github.com>
2019-12-18 17:46:03 +0800

    1

--- src/main/java/com/xmu/wowoto/aftersale/service/OrderService.java
+++ src/main/java/com/xmu/wowoto/aftersale/service/OrderService.java
@@ -1,7 +1,6 @@
 package com.xmu.wowoto.aftersale.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @author MedalWill
  * @date 2019/12/13 19:26
  */
-@Service
 @FeignClient("orderService")
 public interface OrderService {
     
