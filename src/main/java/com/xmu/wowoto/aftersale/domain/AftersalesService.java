95285a08ddc08df5b1372c91e96ddc8aaed494a3
muluoyanran <muluoyanran@users.noreply.github.com>
2019-12-23 02:04:13 +0800

    fff

--- src/main/java/com/xmu/wowoto/aftersale/domain/AftersalesService.java
+++ src/main/java/com/xmu/wowoto/aftersale/domain/AftersalesService.java
@@ -165,57 +165,9 @@ public class AftersalesService {
         this.beDeleted = beDeleted;
     }
 
+    @Override
     public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof AftersalesService)) return false;
-        final AftersalesService other = (AftersalesService) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$id = this.getId();
-        final Object other$id = other.getId();
-        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
-        final Object this$goodsType = this.getGoodsType();
-        final Object other$goodsType = other.getGoodsType();
-        if (this$goodsType == null ? other$goodsType != null : !this$goodsType.equals(other$goodsType)) return false;
-        final Object this$applyTime = this.getApplyTime();
-        final Object other$applyTime = other.getApplyTime();
-        if (this$applyTime == null ? other$applyTime != null : !this$applyTime.equals(other$applyTime)) return false;
-        final Object this$applyReason = this.getApplyReason();
-        final Object other$applyReason = other.getApplyReason();
-        if (this$applyReason == null ? other$applyReason != null : !this$applyReason.equals(other$applyReason))
-            return false;
-        final Object this$endTime = this.getEndTime();
-        final Object other$endTime = other.getEndTime();
-        if (this$endTime == null ? other$endTime != null : !this$endTime.equals(other$endTime)) return false;
-        final Object this$type = this.getType();
-        final Object other$type = other.getType();
-        if (this$type == null ? other$type != null : !this$type.equals(other$type)) return false;
-        final Object this$beApplied = this.getBeApplied();
-        final Object other$beApplied = other.getBeApplied();
-        if (this$beApplied == null ? other$beApplied != null : !this$beApplied.equals(other$beApplied)) return false;
-        final Object this$number = this.getNumber();
-        final Object other$number = other.getNumber();
-        if (this$number == null ? other$number != null : !this$number.equals(other$number)) return false;
-        final Object this$orderItemId = this.getOrderItemId();
-        final Object other$orderItemId = other.getOrderItemId();
-        if (this$orderItemId == null ? other$orderItemId != null : !this$orderItemId.equals(other$orderItemId))
-            return false;
-        final Object this$statusCode = this.getStatusCode();
-        final Object other$statusCode = other.getStatusCode();
-        if (this$statusCode == null ? other$statusCode != null : !this$statusCode.equals(other$statusCode))
-            return false;
-        final Object this$userId = this.getUserId();
-        final Object other$userId = other.getUserId();
-        if (this$userId == null ? other$userId != null : !this$userId.equals(other$userId)) return false;
-        final Object this$gmtCreate = this.getGmtCreate();
-        final Object other$gmtCreate = other.getGmtCreate();
-        if (this$gmtCreate == null ? other$gmtCreate != null : !this$gmtCreate.equals(other$gmtCreate)) return false;
-        final Object this$gmtModified = this.getGmtModified();
-        final Object other$gmtModified = other.getGmtModified();
-        if (this$gmtModified == null ? other$gmtModified != null : !this$gmtModified.equals(other$gmtModified))
-            return false;
-        final Object this$beDeleted = this.getBeDeleted();
-        final Object other$beDeleted = other.getBeDeleted();
-        if (this$beDeleted == null ? other$beDeleted != null : !this$beDeleted.equals(other$beDeleted)) return false;
+
         return true;
     }
 
@@ -223,40 +175,13 @@ public class AftersalesService {
         return other instanceof AftersalesService;
     }
 
+    @Override
     public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $id = this.getId();
-        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-        final Object $goodsType = this.getGoodsType();
-        result = result * PRIME + ($goodsType == null ? 43 : $goodsType.hashCode());
-        final Object $applyTime = this.getApplyTime();
-        result = result * PRIME + ($applyTime == null ? 43 : $applyTime.hashCode());
-        final Object $applyReason = this.getApplyReason();
-        result = result * PRIME + ($applyReason == null ? 43 : $applyReason.hashCode());
-        final Object $endTime = this.getEndTime();
-        result = result * PRIME + ($endTime == null ? 43 : $endTime.hashCode());
-        final Object $type = this.getType();
-        result = result * PRIME + ($type == null ? 43 : $type.hashCode());
-        final Object $beApplied = this.getBeApplied();
-        result = result * PRIME + ($beApplied == null ? 43 : $beApplied.hashCode());
-        final Object $number = this.getNumber();
-        result = result * PRIME + ($number == null ? 43 : $number.hashCode());
-        final Object $orderItemId = this.getOrderItemId();
-        result = result * PRIME + ($orderItemId == null ? 43 : $orderItemId.hashCode());
-        final Object $statusCode = this.getStatusCode();
-        result = result * PRIME + ($statusCode == null ? 43 : $statusCode.hashCode());
-        final Object $userId = this.getUserId();
-        result = result * PRIME + ($userId == null ? 43 : $userId.hashCode());
-        final Object $gmtCreate = this.getGmtCreate();
-        result = result * PRIME + ($gmtCreate == null ? 43 : $gmtCreate.hashCode());
-        final Object $gmtModified = this.getGmtModified();
-        result = result * PRIME + ($gmtModified == null ? 43 : $gmtModified.hashCode());
-        final Object $beDeleted = this.getBeDeleted();
-        result = result * PRIME + ($beDeleted == null ? 43 : $beDeleted.hashCode());
-        return result;
+
+        return 0;
     }
 
+    @Override
     public String toString() {
         return "AftersalesService(id=" + this.getId() + ", goodsType=" + this.getGoodsType() + ", applyTime=" + this.getApplyTime() + ", applyReason=" + this.getApplyReason() + ", endTime=" + this.getEndTime() + ", type=" + this.getType() + ", beApplied=" + this.getBeApplied() + ", number=" + this.getNumber() + ", orderItemId=" + this.getOrderItemId() + ", statusCode=" + this.getStatusCode() + ", userId=" + this.getUserId() + ", gmtCreate=" + this.getGmtCreate() + ", gmtModified=" + this.getGmtModified() + ", beDeleted=" + this.getBeDeleted() + ")";
     }
--- src/main/java/com/xmu/wowoto/aftersale/domain/Log.java
+++ src/main/java/com/xmu/wowoto/aftersale/domain/Log.java
@@ -109,40 +109,9 @@ public class Log {
         this.gmtModified = gmtModified;
     }
 
+    @Override
     public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Log)) return false;
-        final Log other = (Log) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$id = this.getId();
-        final Object other$id = other.getId();
-        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
-        final Object this$adminId = this.getAdminId();
-        final Object other$adminId = other.getAdminId();
-        if (this$adminId == null ? other$adminId != null : !this$adminId.equals(other$adminId)) return false;
-        final Object this$ip = this.getIp();
-        final Object other$ip = other.getIp();
-        if (this$ip == null ? other$ip != null : !this$ip.equals(other$ip)) return false;
-        final Object this$type = this.getType();
-        final Object other$type = other.getType();
-        if (this$type == null ? other$type != null : !this$type.equals(other$type)) return false;
-        final Object this$actions = this.getActions();
-        final Object other$actions = other.getActions();
-        if (this$actions == null ? other$actions != null : !this$actions.equals(other$actions)) return false;
-        final Object this$statusCode = this.getStatusCode();
-        final Object other$statusCode = other.getStatusCode();
-        if (this$statusCode == null ? other$statusCode != null : !this$statusCode.equals(other$statusCode))
-            return false;
-        final Object this$actionId = this.getActionId();
-        final Object other$actionId = other.getActionId();
-        if (this$actionId == null ? other$actionId != null : !this$actionId.equals(other$actionId)) return false;
-        final Object this$gmtCreate = this.getGmtCreate();
-        final Object other$gmtCreate = other.getGmtCreate();
-        if (this$gmtCreate == null ? other$gmtCreate != null : !this$gmtCreate.equals(other$gmtCreate)) return false;
-        final Object this$gmtModified = this.getGmtModified();
-        final Object other$gmtModified = other.getGmtModified();
-        if (this$gmtModified == null ? other$gmtModified != null : !this$gmtModified.equals(other$gmtModified))
-            return false;
+
         return true;
     }
 
@@ -150,30 +119,13 @@ public class Log {
         return other instanceof Log;
     }
 
+    @Override
     public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $id = this.getId();
-        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-        final Object $adminId = this.getAdminId();
-        result = result * PRIME + ($adminId == null ? 43 : $adminId.hashCode());
-        final Object $ip = this.getIp();
-        result = result * PRIME + ($ip == null ? 43 : $ip.hashCode());
-        final Object $type = this.getType();
-        result = result * PRIME + ($type == null ? 43 : $type.hashCode());
-        final Object $actions = this.getActions();
-        result = result * PRIME + ($actions == null ? 43 : $actions.hashCode());
-        final Object $statusCode = this.getStatusCode();
-        result = result * PRIME + ($statusCode == null ? 43 : $statusCode.hashCode());
-        final Object $actionId = this.getActionId();
-        result = result * PRIME + ($actionId == null ? 43 : $actionId.hashCode());
-        final Object $gmtCreate = this.getGmtCreate();
-        result = result * PRIME + ($gmtCreate == null ? 43 : $gmtCreate.hashCode());
-        final Object $gmtModified = this.getGmtModified();
-        result = result * PRIME + ($gmtModified == null ? 43 : $gmtModified.hashCode());
-        return result;
+
+        return 0;
     }
 
+    @Override
     public String toString() {
         return "Log(id=" + this.getId() + ", adminId=" + this.getAdminId() + ", ip=" + this.getIp() + ", type=" + this.getType() + ", actions=" + this.getActions() + ", statusCode=" + this.getStatusCode() + ", actionId=" + this.getActionId() + ", gmtCreate=" + this.getGmtCreate() + ", gmtModified=" + this.getGmtModified() + ")";
     }
